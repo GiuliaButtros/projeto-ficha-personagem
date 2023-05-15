8740ad6b9f75a229b843344cec4144e79d707df5
Giulia Buttros <105403316+GiuliaButtros@users.noreply.github.com>
2023-05-14 21:59:11 -0300

    update script

--- js/att-stats.js
+++ js/att-stats.js
@@ -1,3 +1,7 @@
+
+
+
+
 // Código que roda quando a página é carregada
 const maxAttribute = 5; // Valor máximo de cada atributo
 const maxPoints = 10; // Pontos totais a serem distribuídos
@@ -5,8 +9,123 @@ let pointsToSpend = maxPoints; // Variável iniciada com o valor máximo
 let forcValue = agilValue = inteliValue = 0; // Uma variável para cada atributo, iniciadas em 0
 updatePips(); // Chama a função que atualiza o estado dos pips
 
+//----- ARMAS E STATS
+let arma = "";
+let equipamento = "";
+let dano = 0;
+let defesa = 0;
+let magia = 0;
+let bonusRacaForc = 0;
+let bonusRacaAgil = 0;
+let bonusRacaInteli = 0;
+let bonusDanoArma = 0;
+let bonusDefesaEq = 0;
+let bonusMagiaEq = 0;
+let seletorRaca = document.getElementById("raca-select");
+let seletorClasse = document.getElementById("classe-select");
+let elementoFigura = document.getElementById("figura");
+
+// Texto em estatisticas que fala os bonus da raca
+let textoRaca = document.getElementById("textoRaca");
+
+// Textos em estaticas que falam a arma e o equipamento da classe
+let textoArma = document.getElementById("textoArma");
+let textoEquip = document.getElementById("textoEquip");
+
+// Texto em estatisticas que falam as 3 estatísticas finais
+let textoDano = document.getElementById("textoDano");
+let textoDefesa = document.getElementById("textoDefesa");
+let textoMagia = document.getElementById("textoMagia");
+// -----
+
 
 //Funções
+
+// Bonus raça + equipamentos etc
+
+function updateRacaEClasse () {
+    
+    // Usar no switch o texto do item selecionado em Raca  
+    switch (document.getElementById("raca-select").options[document.getElementById("raca-select").selectedIndex].text) {
+        case "Humano":
+            bonusRacaForc = 0;
+            bonusRacaAgil = 0;
+            bonusRacaInte = 0;
+            // Talvez seja outra funcao em vez de innerText
+            textoRaca.innerHTML="Humano: Sem bonus";
+            break;
+        case "Elfo":
+            bonusRacaForc = -1;
+            bonusRacaAgil = 0;
+            bonusRacaInte = 1;
+            textoRaca.innerHTML = "Elfo: -1 em Força, +1 em Inteligência";
+            break;
+        case "Anao":
+            bonusRacaForc = 1;
+            bonusRacaAgil = -1;
+            bonusRacaInte = 0;
+            textoRaca.innerHTML="Anão: +1 em Força, -1 em Agilidade";
+            break;
+        case "Hobbit":
+            bonusRacaForc = -1;
+            bonusRacaAgil = 1;
+            bonusRacaInte = 0;
+            textoRaca.innerHTML="Hobbit: -1 em Força, +1 em Agilidade";
+            break;
+        default:
+            console.log("Erro no switch 1 em updateRacaEClasse");
+            break;
+    }
+
+    console.log(bonusRacaForc);
+
+    // Usar no switch o texto do item selecionado em Classe
+    switch (seletorClasse.options[seletorRaca.selectedIndex].text) {
+        case "Guerreiro":
+            arma = "Espada";
+            bonusDanoArma = 2;
+            equipamento = "Armadura média";
+            bonusDefesaEq = 3;
+            bonusMagiaEq = 0;
+            break;
+        case "Ladino":
+            arma = "Adaga";
+            bonusDanoArma = 1;
+            equipamento = "Armadura leve";
+            bonusDefesaEq = 1;
+            bonusMagiaEq = 0;
+            break;
+        case "Mago":
+            arma = "Cajado";
+            bonusDanoArma = 1;
+            equipamento = "Anel mágico";
+            bonusDefesaEq = 0;
+            bonusMagiaEq = 1;
+            break;
+        default:
+            console.log("Erro no switch 2 em updateRacaEClasse");
+            break;
+    }
+
+    // Troca o texto da arma e do equipamento
+    textoArma.innerText(arma);
+    textoEquip.innerText(equipamento);
+
+    // Troca a figura. Pode ter que mexer no começo da url
+    elementoFigura.style.backgroundImage="url(..imagens/" + raca + "_" + classe + ".PNG)";
+    
+    updateEstatisticas();   
+}
+
+function updateEstatisticas() {   
+    dano = forcValue + bonusRacaForc + bonusDanoArma;
+    defesa = agilValue + bonusRacaAgil + bonusDefesaEq;
+    magia = inteliValue + bonusRacaInte + bonusMagiaEq;
+
+    textoDano.innerText(dano);
+    textoDefesa.innerText(defesa);
+    textoMagia.innerText(magia);
+}
 
 // - Atualiza os pips >
 
@@ -126,6 +245,8 @@ function changeValue(att, value){
         }
     }
     updatePips(); /* Atualiza pips no dinal da mudança das variáveis */
+
+    updateEstatisticas();
 }
 
 // Acende o pip referente ao id recebido como parâmetro (string) >
@@ -137,6 +258,7 @@ function turnOn (id){
 // Apaga pip referente ao id recebido como parâmetro (string) >
 
 function turnOff(id){
+    console.log(id);
     document.getElementById(id).style.backgroundColor = "rgb(163, 149, 129)";
 }
 
